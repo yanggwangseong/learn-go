44bd0dd01f4971f689ab00d6f549da4a82dbff3e
YangGwangSeong <rhkdtjd_12@naver.com>
2025-06-06 23:26:04 +0900

    feat: struct literals

--- basic01/struct_example/struct.go
+++ basic01/struct_example/struct.go
@@ -18,11 +18,21 @@ type Vertex struct {
 // 	fmt.Println(v.x) // 10
 // }
 
-// Pointers to structs
+// ****Pointers to structs****
+// func main(){
+// 	v := Vertex{1, 2}
+// 	p := &v
+// 	p.x = 1e9
+// 	fmt.Println(v) // {1000000000 2}
+// }
 
+// ****Struct Literals****
+var (
+	v1 = Vertex{1, 2}
+	v2 = Vertex{x: 1}
+	v3 = Vertex{}
+	p = &Vertex{1, 2}
+)
 func main(){
-	v := Vertex{1, 2}
-	p := &v
-	p.x = 1e9
-	fmt.Println(v) // {1000000000 2}
+	fmt.Println(v1, p, v2, v3)
 }
